f8859ccbdadde7a1fcfa63c8a5ff6a9d17d9de08
Денис Украинский <denisukrainskii123@gmail.com>
2024-08-24 20:12:17 +0300

    Добавил две функции по второму дз

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	const (
-		usdEuro = 0.91
-		usdRuble = 91.37
+		usdEuro   = 0.91
+		usdRuble  = 91.37
 		euroRuble = usdRuble / usdEuro
 	)
-	fmt.Printf("%v %v %.2f", usdEuro, usdRuble, euroRuble)
+	getUserInput()
+	convertCurrency(4, "", "")
 }
+
+func getUserInput() {
+	// var currentCurrency, targetCurrency float64
+	var someString string
+	fmt.Scan(&someString)
+	fmt.Print(someString)
+}
+
+func convertCurrency(c int, a string, b string) {}
