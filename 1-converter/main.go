5e726c6e81aea9d9bdb745f0b912b53ebcf37d72
Денис Украинский <denisukrainskii123@gmail.com>
2024-09-01 20:26:31 +0300

    Закончил рефакторинг кода

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strconv"
+	"strings"
 )
 
-const (
-	usdEuro   = 0.91
-	usdRuble  = 91.37
-	euroRuble = usdRuble / usdEuro
-)
+type currencyMap = map[string]map[string]float64
 
 func main() {
+	currencyMap := currencyMap{
+		"USD": map[string]float64{
+			"Euro":  0.91,
+			"Ruble": 91.37},
+	}
+	currencyMap["Euro"] = make(map[string]float64, 2)
+	currencyMap["Euro"]["USD"] = 1 / currencyMap["USD"]["Euro"]
+	currencyMap["Euro"]["Ruble"] = currencyMap["USD"]["Ruble"] / currencyMap["USD"]["Euro"]
+	currencyMap["Ruble"] = make(map[string]float64, 2)
+	currencyMap["Ruble"]["USD"] = 1 / currencyMap["USD"]["Ruble"]
+	currencyMap["Ruble"]["Euro"] = 1 / currencyMap["Euro"]["Ruble"]
 	fmt.Println("__Конверетр валют__")
 	fmt.Println("Меню:")
 	fmt.Println("1. Введите исходную валюту;")
@@ -20,120 +26,42 @@ func main() {
 	fmt.Println("3. Введите целевую валюту.")
 	fmt.Printf("\n")
 	for {
-		currentCurrancy, number, targetCurrency, err := getUserInput()
-		if err != nil {
-			fmt.Printf("Ошибка: %v\n", err)
-			continue
-		}
-		result, _ := convertCurrency(currentCurrancy, number, targetCurrency)
-		fmt.Printf("Результат:%v %v=%.2f %v\n", number, currentCurrancy, result, targetCurrency)
+		var currentCurrancy, targetCurrancy string
+		var number float64
+		fmt.Print("1. (Euro, USD или Ruble) ")
+		fmt.Scan(&currentCurrancy)
+		fmt.Print("2. ")
+		fmt.Scan(&number)
+		getTargetCurrency(&currentCurrancy, &targetCurrancy, currencyMap)
+		result, _ := convertCurrency(currentCurrancy, number, targetCurrancy, currencyMap)
+		fmt.Printf("Результат: %.2F %v\n", result, targetCurrancy)
 		if !wantToContinue() {
 			break
 		}
 	}
 }
 
-func getUserInput() (string, float64, string, error) {
-
-	currentCurrancy, err := getCurrentCurrency()
-	for err != nil {
-		return "", 0, "", err
-	}
-	number, err := getNumber()
-	if err != nil {
-		return "", number, "", err
-	}
-	curA, curB, err := returnRemainingCurrencies(currentCurrancy)
-	if err != nil {
-		return "", 0, "", err
-	}
-	targetCurrency, err := getTargetCurrency(curA, curB)
-	if err != nil {
-		return "", 0, "", err
-	}
-	return currentCurrancy, number, targetCurrency, nil
-}
-
-func getCurrentCurrency() (string, error) {
-	var currentCurrency string
-	fmt.Print("1. Введите исходную валюту (USD, Euro или Ruble): ")
-	fmt.Scan(&currentCurrency)
-	if currentCurrency == "USD" || currentCurrency == "Euro" || currentCurrency == "Ruble" {
-		return currentCurrency, nil
-	} else {
-		return "", errors.New("вы пытаетесь ввести валюту, которой у нас нет")
-	}
-}
-
-func returnRemainingCurrencies(currentCurrency string) (string, string, error) {
-	switch currentCurrency {
-	case "USD":
-		return "Euro", "Ruble", nil
-	case "Euro":
-		return "USD", "Ruble", nil
-	case "Ruble":
-		return "USD", "Euro", nil
-	default:
-		return "", "", errors.New("получена неверная валюта для сравнения")
-	}
-}
-
-func getTargetCurrency(currencyA string, currencyB string) (string, error) {
-	var targetCurrency string
-	fmt.Printf("3. Введите целевую валюту (%v или %v): ", currencyA, currencyB)
-	if fmt.Scan(&targetCurrency); targetCurrency == currencyA || targetCurrency == currencyB {
-		return targetCurrency, nil
-	}
-	return "", errors.New("введена неверная целевая валюта")
-}
-
-func getNumber() (float64, error) {
-	var numberStr string
-	fmt.Print("3. Введите число: ")
-	fmt.Scan(&numberStr)
-	num, err := strconv.Atoi(numberStr)
-	if err != nil {
-		return 0, errors.New("введено не число")
-	}
-	number := float64(num)
-	if number <= 0 {
-
-		return 0, errors.New("введено некорректное число")
-	}
-	return number, nil
-}
-
-func convertCurrency(currentCurrancy string, number float64, targetCurrancy string) (float64, error) {
-	switch currentCurrancy {
-	case "USD":
-		if targetCurrancy == "Euro" {
-			return number * usdEuro, nil
-		} else if targetCurrancy == "Ruble" {
-			return number * usdRuble, nil
-		}
-	case "Euro":
-		if targetCurrancy == "USD" {
-			return number / usdEuro, nil
-		} else if targetCurrancy == "Ruble" {
-			return number * euroRuble, nil
-		}
-	case "Ruble":
-		if targetCurrancy == "USD" {
-			return number / usdRuble, nil
-		} else if targetCurrancy == "Euro" {
-			return number / euroRuble, nil
-		}
-	}
-	return 0, errors.New("пытаетесь конверитировать несуществуюую валюту")
+func convertCurrency(currentCurrancy string, number float64, targetCurrancy string, currencies currencyMap) (float64, error) {
+	return currencies[currentCurrancy][targetCurrancy] * number, nil
 }
 
 func wantToContinue() bool {
 	var userAnswer string
-	fmt.Print("Желаете еще конверировать валюту? (Y/N)")
+	fmt.Print("Желаете еще конверировать валюту? (Y/N) ")
 	fmt.Scan(&userAnswer)
-	fmt.Printf("\n")
 	if userAnswer == "Y" || userAnswer == "y" {
 		return true
 	}
 	return false
+}
+
+func getTargetCurrency(currentCurrancy *string, targetCurrancy *string, currencyMap currencyMap) {
+	str := ""
+	for key := range currencyMap[*currentCurrancy] {
+		str += key
+		str += " или "
+	}
+	str = strings.TrimRight(str, "ил ")
+	fmt.Printf("3. (%v) ", str)
+	fmt.Scan(targetCurrancy)
 }
